ca01ce5a741620592dfc1f8899f68cf644fcbc0e
Andrew Randan <andrewrandan@gmail.com>
2019-12-03 16:55:59 +0200

    added logging

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const sequelize = require('./db');
 const handleErrors = require('./middlewares/error');
+const logging = require('./middlewares/logging');
 
 const subscribersRouter = require('./routes/subscribers');
 
 app.use(express.json());
+
+app.use(handleErrors);
+app.use(logging);
 
 app.use('/subscribers', subscribersRouter);
 
@@ -13,6 +17,5 @@ sequelize.sync()
     .then(() => console.log('database connected'))
     .catch(err => console.log(err));
 
-app.use(handleErrors);
 
 app.listen(3000, () => console.log('server started'));
